d8b83e000f369aa023e920a876fb84b65426d813
Wonmin Cho <0108257@gmail.com>
2021-04-23 19:30:03 +0900

    Lucky

---
+++ Lucky-Coin/Assets/Script/Star.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class Star : MonoBehaviour
+{
+    private Tilemap tileMap;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        tileMap = GetComponent<Tilemap>();
+        // Twinkle();
+    }
+
+    private void Update()
+    {
+    }
+
+    private void Twinkle()
+    {
+        Debug.Log("Twinkle");
+        if (tileMap.color.a == 1)
+        {
+            tileMap.color = new Vector4(1, 1, 1, 0);
+        } else
+        {
+            tileMap.color = new Vector4(1, 1, 1, 1);
+        }
+        
+        Invoke("Twinkle", 1);
+        
+    }
+
+}
