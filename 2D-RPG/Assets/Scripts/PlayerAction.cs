a0fe709e1a10e6865d311dbddb17db65b416b41e
Wonmin Cho <0108257@gmail.com>
2021-07-18 01:18:24 +0900

    PlayerAction script

---
+++ 2D-RPG/Assets/Scripts/PlayerAction.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAction : MonoBehaviour
+{
+    float h;
+    float v;
+
+    Rigidbody2D rigidbody;
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        h = Input.GetAxisRaw("Horizontal");
+        v = Input.GetAxisRaw("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        rigidbody.velocity = new Vector2(h, v);
+    }
+}
